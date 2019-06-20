ba74c11ea5edffd38c56817457de9a601ec20a48
Doby <dyxdyxdyxdyxdyx@outlook.com>
2019-06-20 10:41:11 +0800

    删除文件

--- 28377_weizhihuan/F2837xS_examples_Cpu1/sci_loopback/cpu01/ccs/CPU1_FLASH/optimizerAssistant/opt_level__0/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"2837xS_Generic_FLASH_lnk.cmd" -l"F2837xS_Headers_nonBIOS.cmd" -l"libc.a"
-
